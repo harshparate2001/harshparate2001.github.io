0c218e328b819d8684db64c7f376c74ec0f05298
Harsh Parate <harshparate315@gmail.com>
2025-10-18 10:49:48 +0530

    Deploy actions added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-      plugins: [tailwindcss()],
-  },
+    site: 'https://harshparate2001.github.io',
+    vite: {
+        plugins: [tailwindcss()],
+    },
 
-  integrations: [react()]
+    integrations: [react()]
 });
